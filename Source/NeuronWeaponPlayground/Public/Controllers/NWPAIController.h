37b61e5cbec9673d13e8ad835156d8ef7a530131
NeuronStation <neuron.station@gmail.com>
2019-10-29 13:29:39 +0100

    Added NWPPlayerController, NWPAIController, BP_PlayerController & BP_AIController

---
+++ Source/NeuronWeaponPlayground/Public/Controllers/NWPAIController.h
@@ -0,0 +1,17 @@
+// Copyright 2019 Neuron Station. All Rights Reserved.
+
+#pragma once
+
+// Generated
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "NWPAIController.generated.h"
+
+/**
+ * The AI controller that controls the character.
+ */
+UCLASS()
+class NEURONWEAPONPLAYGROUND_API ANWPAIController : public AAIController
+{
+	GENERATED_BODY()
+};
---
+++ Source/NeuronWeaponPlayground/Public/Controllers/NWPPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright 2019 Neuron Station. All Rights Reserved.
+
+#pragma once
+
+// Generated
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NWPPlayerController.generated.h"
+
+/**
+ * The player controller that controls the character.
+ */
+UCLASS()
+class NEURONWEAPONPLAYGROUND_API ANWPPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
